0d2d0bc1e734314a1a542b9cd2119283708a9d92
Faith Etornam <faithgbadegbe1@gmail.com>
2025-09-28 22:01:50 +0000

    Made a few changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,5 @@
-def perform_operation(num1: float, num2: float, operation: str) -> float:
+
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -7,9 +8,8 @@ def perform_operation(num1: float, num2: float, operation: str) -> float:
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-            return 'Division by zero is invalid'
+            return "Error: Division by zero is not allowed"
         else:
             return num1 / num2
-        
-
-    
+    else:
+        return "Error: Invalid operation"
