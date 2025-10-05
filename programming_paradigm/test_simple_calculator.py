ddd016fd3ca7dd6c0d0a167567a4313681216208
Faith Etornam <faithgbadegbe1@gmail.com>
2025-10-05 21:37:48 +0000

    Added a test to a calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,22 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(6, 3), 2.0)
+
+    def test_division_by_zero(self):
+        self.assertIsNone(self.calc.divide(5, 0))
